c31d7b5a0af4fec097e95a9653aa2b94daa3f8a4
iknowball <morrisjack3131@gmail.com>
2025-09-24 19:34:39 -0400

    Initialize Firebase and Firestore in firebase.js

---
+++ app/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtsOBlh52YZagsLXp9_dcCq4qhkHBSWnU",
+  authDomain: "thepub-sigma.firebaseapp.com",
+  projectId: "thepub-sigma",
+  storageBucket: "thepub-sigma.appspot.com",
+  messagingSenderId: "108972507038",
+  appId: "1:108972507038:web:4503cbe63cb3f7a3e4e08f"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
