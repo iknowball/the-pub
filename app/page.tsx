b9ddf624e5eccb81b730d669af8f237a22ff2ad6
iknowball <morrisjack3131@gmail.com>
2025-09-28 10:28:24 -0400

    Fix import path for Firebase instance

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
 import Link from "next/link";
-import { auth, db } from "../firebase"; // <-- Use shared Firebase instance
+import { auth, db } from "./firebase"; // <-- Use shared Firebase instance
 import {
   onAuthStateChanged,
   signInWithEmailAndPassword,
