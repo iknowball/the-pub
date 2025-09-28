194670b5d017cdcbd5392add37ff7c1c22243f5f
iknowball <morrisjack3131@gmail.com>
2025-09-28 10:01:24 -0400

    Create firebase.ts

---
+++ app/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtsOBlh52YZagsLXp9_dcCq4qhkHBSWnU",
+  authDomain: "thepub-sigma.firebaseapp.com",
+  projectId: "thepub-sigma",
+};
+
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
